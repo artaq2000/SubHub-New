53c60f190237ce529e4d8fbb06fea06f3ae9227c
artaq2000 <artaq2000@gmail.com>
2026-09-24 13:50:31 +0300

    Stamp bridge 322.3.5

--- android-bridge/app/src/main/assets/site_bridge.js
+++ android-bridge/app/src/main/assets/site_bridge.js
@@ -3,7 +3,7 @@
   if (window.__subHubSiteBridgeV223) return true;
   window.__subHubSiteBridgeV223 = true;
 
-  const BRIDGE_BUILD = '322.3.4';
+  const BRIDGE_BUILD = '322.3.5';
 
   let lastSig = '';
   let clockSource = '';
